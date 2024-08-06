81dda00f6b0eab88af40e4067d899c4e261b9f21
Alisha Nusrat <165286121+alishanusrat@users.noreply.github.com>
2024-08-06 17:36:40 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/*example1*/
+
+function showText1(){
+	document.getElementById('a_text').innerHTML="Hello SSB";
+}
+function showText2(){
+	document.getElementById('b_text').innerHTML="Hello 619";
+}
+
+function showText3(){
+	document.getElementById('c_text').innerHTML="Hello world";
+}
+function showText4(){
+	document.getElementById('d_text').innerHTML="Hello BD";
+}
+function showText5(){
+	document.getElementById('e_text').innerHTML="Hello Hasi";
+}
+function showText6(){
+	document.getElementById('f_text').innerHTML="Hello na";
+}
+function showText7(){
+	document.getElementById('g_text').innerHTML="Hello students";
+}
+function showText8(){
+	document.getElementById('h_text').innerHTML="Hello In";
+}
+function showText9(){
+	document.getElementById('i_text').innerHTML="Hello dia";
+}
+function showText10(){
+	document.getElementById('j_text').innerHTML="Hello hasu";
+}
